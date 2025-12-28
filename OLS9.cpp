1f9dee3646a01837018e07db64d336f61e609dd1
Ayush Kumar Singh <ayushkumares@gmail.com>
2025-12-28 14:24:32 +0530

    Solved 1 problem

--- OLS9.cpp
+++ OLS9.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
 #define ll long long
 
-void problemA();
+void problemA(); // this is wrong
+void problemB();
 
 int main()
 {
-    problemA();
+    problemB();
     return 0;
 }
 
@@ -33,4 +35,42 @@ void problemA()
             break;
     }
     cout << ans << "\n";
+}
+
+void problemB()
+{
+    int n, q;
+    cin >> n >> q;
+    string s;
+    cin >> s;
+    vector<int> prevC(n), nextC(n), ps(n + 1, 0);
+    int curr = -1;
+    for (int i = 0; i < n; i++)
+    {
+        ps[i + 1] = ps[i] + (s[i] == '*');
+        curr = s[i] == '|' ? i : curr;
+        prevC[i] = curr;
+    }
+    curr = n;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        curr = s[i] == '|' ? i : curr;
+        nextC[i] = curr;
+    }
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+        assert(l >= 0 && r < n);
+        if (nextC[l] == n || prevC[r] == -1)
+        {
+            cout << 0 << "\n";
+        }
+        else
+        {
+            int a = nextC[l];
+            int b = prevC[r];
+            cout << ps[b] - ps[a + 1] << "\n";
+        }
+    }
 }
