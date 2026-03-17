58dc792f16db36a1ab3dfca1dbd99c1a6446efe9
Ayush Kumar Singh <ayushkumares@gmail.com>
2026-03-17 10:32:58 +0530

    probabluy can be improved but ifitworksitworks

---
+++ LAB/9/Q1.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node_t
+{
+    int data;
+    struct Node_t *left;
+    struct Node_t *right;
+} Node;
+
+Node *createNode(int val)
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node *findParent(Node *root, int val)
+{
+    if (!root)
+        return NULL;
+    if (root->data > val && root->left == NULL)
+        return root;
+    else if (root->data < val && root->right == NULL)
+        return root;
+    else if (root->data > val)
+        return findParent(root->left, val);
+    else if (root->data < val)
+        return findParent(root->right, val);
+}
+
+Node *insert(Node *root, int val)
+{
+    Node *newNode = createNode(val);
+    if (!root)
+        return newNode;
+    Node *parent = findParent(root, val);
+    if (val < parent->data)
+        parent->left = newNode;
+    else
+        parent->right = newNode;
+    return root;
+}
+
+void inorder(Node *root)
+{
+    if (!root)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+    return;
+}
+
+Node *buildBST(int *a, int n)
+{
+    Node *root = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        root = insert(root, a[i]);
+    }
+    return root;
+}
+
+int find(Node* root, int x, int* path, int idx){
+    if(!root){
+        return -1;
+    }
+    path[idx++] = root->data;
+    if(x==root->data){
+        return idx;
+    }
+    else if(x < root->data){
+        find(root->left, x, path, idx);
+    }else{
+        find(root->right, x, path, idx);
+    }
+}
+
+int LCA(Node* root, int a, int b){
+    int A[1000];
+    int B[1000];
+    int aa = find(root, a, A, 0);
+    int bb = find(root, b, B, 0);
+    int ans = root->data;
+    for(int i=0; i<aa && i<bb; i++){
+        if(A[i]==B[i]) ans = A[i];
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int a,b;
+    scanf("%d %d", &a, &b);
+    Node *root = buildBST(arr, n);
+    int lca = LCA(root, a,b);
+    printf("%d\n", lca);
+    return 0;
+}
